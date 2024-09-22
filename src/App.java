b90ec663778d163425096bd8a6bb2ab3e32a5b1d
imlakshay <kalamkarakshay2001@gmail.com>
2024-09-22 20:58:49 +0530

    new code

---
+++ src/App.java
@@ -0,0 +1,6 @@
+import Arrays.*;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello akshay welcome to the java world");
+    }
+}
---
+++ src/Arrays/Hello.java
@@ -0,0 +1,8 @@
+package Arrays;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+  
+}
