2d94cf7e994e782f1ecaa9fb58bd0d47ae6141c9
imlakshay <kalamkarakshay2001@gmail.com>
2024-09-23 16:01:48 +0530

    java arrays and multi dimentional arrays

--- src/App.java
+++ src/App.java
@@ -1,6 +1,64 @@
-import Arrays.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello akshay welcome to the java world");
+    //    int marks[]={98,12,45,15,65};
+    //    System.out.println(marks[0]);
+
+    // String names[]= {"akshay","sunil","kalamkar"};
+    // print all the elements which is present in an array
+    // for (int i = 0; i < names.length; i++) {
+    //     System.out.println("name is "+names[i]);
+    // }
+
+    /// for each
+    // for (String name: names) {
+    //     System.out.println(name);
+    // }
+
+    // sum of the all number in array
+    // int numbers[]={1,2,3,4,5};
+    // int sum=0;
+    // for (int num : numbers) {
+    //     sum=sum+num;
+    // }
+    // System.out.println(sum);
+
+
+
+
+    // smallest elements in an array
+    // int numbers[]={515,429,35,44,75};
+    // int min=Integer.MAX_VALUE;
+    // for (int number : numbers) {
+    //     if(number < min){ ///   
+    //         min = number;
+    //     }
+    // }
+    // System.out.println("minimun is "+ min);
+
+
+    // largest elements in an array
+    // int numbers[]={5,4,6,7,8,44,2,9};
+    // int max=Integer.MIN_VALUE;
+    // for (int number : numbers) {
+    //    if (number > max) {
+    //     max=number;
+    //    }
+    // }
+    // System.out.println("maximum number is "+max);
+
+    /////// multi dimentional array
+     
+    int marks[][]={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {10,11,12},
+    };
+    System.out.println(marks[3][2]);
+
+        
+
+
     }
 }
