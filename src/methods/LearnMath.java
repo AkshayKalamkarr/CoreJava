b6052435777bf28ced00aec4987116dab69eee9f
imlakshay <kalamkarakshay2001@gmail.com>
2024-09-24 11:06:20 +0530

    math.random

--- src/methods/LearnMath.java
+++ src/methods/LearnMath.java
@@ -4,9 +4,15 @@ public class LearnMath {
  
   public static void main(String[] args) {
 
-    int a=4;
-    int b=7;
-    System.out.println(Math.min(a, b));
+    // int a=3;
+    // int b=4;
+    // System.out.println(Math.pow(a, b));
+    System.out.println(getRandom(10,20));
+  }
+
+
+  public static int getRandom(int a,int b){
+    return (int) (Math.random()*(b-a+1)+a);
   }
   
 }
