44c343256d6abbc3569acd991f2a8511157e3846
imlakshay <kalamkarakshay2001@gmail.com>
2024-09-24 16:15:15 +0530

    Oops concept

---
+++ src/oops/LearningClasses.java
@@ -0,0 +1,47 @@
+package oops;
+
+public class LearningClasses {
+  public static void main(String[] args) {
+    // Dog dog =new Dog();
+    // dog.name="Tommy";
+    // dog.walk();
+
+    // Dog dog2=new Dog();
+    // dog2.name="Leo";
+    // dog.bark();
+
+
+
+
+  }
+  
+}
+
+
+// class Dog{   /// factory 
+//   String name;
+//   String age;
+//   String color;
+
+//   void walk(){
+//     System.out.println(name+" is walking");
+//   }
+
+//   void bark(){
+//     System.out.println(name+" is barking");
+//   }
+// }
+
+// class Cat{
+// } 
+
+
+class Complex{
+  int a,b;
+
+  void print(){
+    System.out.println();
+  }
+}
+
+
