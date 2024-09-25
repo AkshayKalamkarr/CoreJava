01bb25a4d4ab6a5b57f7c97992aeab9ebc736f4b
imlakshay <kalamkarakshay2001@gmail.com>
2024-09-25 13:00:53 +0530

    method overloading examples

--- src/oops/Methodoverloading.java
+++ src/oops/Methodoverloading.java
@@ -6,7 +6,8 @@ public static void main(String[] args) {
   Greet greet=new Greet();
   greet.greetings();
   greet.greetings("Akshay");
-
+  greet.greetings(5);
+  greet.greetings("herambh ",8);
 }
 }
 
@@ -17,10 +18,23 @@ class Greet{
   }
 
 
+  void greetings(int min){
+    System.out.println(min+" min remaining for morning");
+  }
+
   void greetings(String name){
     System.out.println(name+" Hello GOod Morning ");
   }
+
+  void greetings(String name,int min){
+    System.out.println("hello "+name+" "+min+" remaining for morning");
+  }
+
+
+
 }
+
+
 
 
 
