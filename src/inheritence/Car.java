4aca536849fe31f75a55dbe9ec29a8691b33182e
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-09 17:19:35 +0530

    this and super

--- src/inheritence/Car.java
+++ src/inheritence/Car.java
@@ -2,10 +2,25 @@ package inheritence;
 
 public class Car extends Vehicle {
 
+  String color;
+
   void start(){
-    System.out.println("car is starting");
+    Scooter scooter=new Scooter();
+    scooter.braking(this);
+    super.start();
+    System.out.println(this);
+    System.out.println(this.model+" car is starting");
+  }   
+
+  Car braking(){
+    return this;
   }
 
+
+  Car(){
+    super(3);
+    System.out.println("Car is being created");
+  }
 
 
 
@@ -13,6 +28,14 @@ public class Car extends Vehicle {
   public static void main(String[] args) {
     Car car=new Car();
     car.wheelsCount=4;
+    car.color="black";
     car.start();
+  }
+}
+
+
+class Scooter{
+  void braking(Car car){
+    System.out.println(car.model+" is braking");
   }
 }
--- src/inheritence/Vehicle.java
+++ src/inheritence/Vehicle.java
@@ -3,7 +3,22 @@ package inheritence;
 public class Vehicle {
   int wheelsCount;
 
+  String model="Land cruiser";
+
+
+  Vehicle(){
+    System.out.println("creating a vehicle instance");
+  }
+
+  Vehicle(int wheelsCount){
+    this.wheelsCount=wheelsCount;
+    System.out.println("creating vehicle with wheels");
+  }
+
   void start(){
     System.out.println("vehicle is starting");
   }
 }
+
+
+
