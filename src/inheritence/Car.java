0fe07a52e47796e3a5b206970b87c32d0515673c
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-09 14:56:15 +0530

    mothod overriding

--- src/inheritence/Car.java
+++ src/inheritence/Car.java
@@ -1,6 +1,15 @@
 package inheritence;
 
 public class Car extends Vehicle {
+
+  void start(){
+    System.out.println("car is starting");
+  }
+
+
+
+
+
   public static void main(String[] args) {
     Car car=new Car();
     car.wheelsCount=4;
