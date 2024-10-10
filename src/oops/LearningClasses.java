7ef66f2774016bd3abe24fd5e194093180c7fa0d
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-10 14:34:35 +0530

    new code

--- src/oops/LearningClasses.java
+++ src/oops/LearningClasses.java
@@ -1,52 +1,52 @@
-package oops;
+// package oops;
 
-public class LearningClasses {
-  public static void main(String[] args) {
-    // Dog dog =new Dog();
-    // dog.name="Tommy";
-    // dog.walk();
+// public class LearningClasses {
+//   public static void main(String[] args) {
+//     // Dog dog =new Dog();
+//     // dog.name="Tommy";
+//     // dog.walk();
 
-    // Dog dog2=new Dog();
-    // dog2.name="Leo";
-    // dog.bark();
+//     // Dog dog2=new Dog();
+//     // dog2.name="Leo";
+//     // dog.bark();
 
-    Complex complex=new Complex();
-    complex.a=3;
-    complex.b=4;
-    complex.print();
-
-
+//     Complex complex=new Complex();
+//     complex.a=3;
+//     complex.b=4;
+//     complex.print();
 
 
-  }
+
+
+//   }
   
-}
-
-
-// class Dog{   /// factory 
-//   String name;
-//   String age;
-//   String color;
-
-//   void walk(){
-//     System.out.println(name+" is walking");
-//   }
-
-//   void bark(){
-//     System.out.println(name+" is barking");
-//   }
 // }
 
-// class Cat{
-// } 
+
+// // class Dog{   /// factory 
+// //   String name;
+// //   String age;
+// //   String color;
+
+// //   void walk(){
+// //     System.out.println(name+" is walking");
+// //   }
+
+// //   void bark(){
+// //     System.out.println(name+" is barking");
+// //   }
+// // }
+
+// // class Cat{
+// // } 
 
 
-class Complex{
-  int a,b;
+// // class Complex{
+// //   int a,b;
 
-  void print(){
-    System.out.println(a+" + "+b);
-  }
-}
+// //   void print(){
+// //     System.out.println(a+" + "+b);
+// //   }
+// // }
 
 
