207939afc3586971539189b782ed386f85ca5058
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-11 15:20:52 +0530

    Anonymous Class

---
+++ src/abstraction/LearnAnonymousClasses.java
@@ -0,0 +1,30 @@
+package abstraction;
+
+public class LearnAnonymousClasses {
+
+OuterClass obj=new OuterClass(){
+  void sing(){
+
+  }
+
+  public void outerMethod(){
+
+  }
+}; 
+
+// SuperInterface obj2=new SuperInterface() {
+    
+// };           ///////// craeted the object of the interfaces
+}
+
+
+class OuterClass{
+  public void outerMethod(){
+
+  }
+}
+
+
+interface SuperInterface{
+  void interfaceMethod();
+}
