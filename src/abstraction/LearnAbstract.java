3851e4e16286cc9e3757ef08bae8b12dd5ebb3c2
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-11 11:48:59 +0530

    learning abstract class

---
+++ src/abstraction/LearnAbstract.java
@@ -0,0 +1,38 @@
+package abstraction;
+
+public class LearnAbstract {
+  public static void main(String[] args) {
+    // vehicle vehicle1=new vehicle();
+
+    Car car=new Car();
+    car.accelerate();
+    car.breake(2);
+
+
+  }
+}
+
+
+
+abstract class Vehicle{
+  abstract void accelerate();
+
+  abstract int breake(int wheels);
+}
+
+
+class Car extends Vehicle{
+  void accelerate(){
+    System.out.println("Car is accelerating");
+  }
+
+  void accelerated(){
+
+  }
+
+
+  int breake(int wheels){
+    System.out.println("car breaks are pushed");
+    return wheels;
+  }
+}
