25ccab1a1692f3b0aa4ac1b2250c485ed12ed51c
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-14 17:38:43 +0530

    new code

--- src/polymorphism/LearnObjectClass.java
+++ src/polymorphism/LearnObjectClass.java
@@ -16,11 +16,27 @@ class Car{
   //   return "Car model is "+model+" and year is "+year;
   // }
 
-
+///// override equals method
+  @Override
   public boolean equals(Object obj){
     Car that=(Car)obj;
     return (this.model.equals(that.model) && this.year == that.year) ? true : false;
   }
+
+  // @Override
+  // public int hashcode(){
+  //   int initialNumber=31;
+  //   initialNumber+=year;
+  //   initialNumber+=model.hashCode();
+  //   return initialNumber;
+  // }
+
+
+
+
+
+
+
 }
 
 public class LearnObjectClass {
@@ -30,8 +46,8 @@ public class LearnObjectClass {
 
     System.out.println(obj1.equals(obj2));
     System.out.println(obj1 == obj2);
-    System.out.println(obj1);
-    System.out.println(obj2);  
+    System.out.println(obj1.hashCode());
+    System.out.println(obj2.hashCode());  
   }
   
 }
