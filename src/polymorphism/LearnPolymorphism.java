2bbded3044f829055e192d9b35a787d9567c8014
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-15 16:27:42 +0530

    polymorphism done

--- src/polymorphism/LearnPolymorphism.java
+++ src/polymorphism/LearnPolymorphism.java
@@ -5,18 +5,33 @@ package polymorphism;
 
 class Data{
   int data;
+
+
+  public void printData(){
+    System.out.println(data);
+  }
+}
+
+
+class childData extends Data{
+  @Override
+  public void printData(){
+    System.out.println("overriden "+data);
+  }
 }
 
 public class LearnPolymorphism {
   public static void main(String[] args) {
-    int a=5;
-
-
-    Data obj=new Data();
-    obj.data=5;
-    changeValue(a,obj);
-    System.out.println(a);
-    System.out.println(obj.data);
+    // Data d;
+    // d=new 
+    
+    
+    // int a=5;
+    // Data obj=new Data();
+    // obj.data=5;
+    // changeValue(a,obj);
+    // System.out.println(a);
+    // System.out.println(obj.data);
   }
 
 
