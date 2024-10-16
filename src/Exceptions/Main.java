ec52d6bc34a4bddc74e17cc3499e9e349c159608
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-16 15:24:32 +0530

    java exception

---
+++ src/Exceptions/Main.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class Main {
+  public static void main(String[] args) {
+    try{
+    int a[]=new int [5];
+
+    System.out.println(a[8]);
+  }catch(Exception e){
+    System.out.println("Hello guys");
+  }
+}
+}
