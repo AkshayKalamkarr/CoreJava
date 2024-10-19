0ec3a5b0ab63acd56ff3d0b6b7fc73244f8a4a41
imlakshay <kalamkarakshay2001@gmail.com>
2024-10-19 17:00:42 +0530

    new code

--- src/Exceptions/Main.java
+++ src/Exceptions/Main.java
@@ -5,7 +5,7 @@ public class Main {
     try{
     int a[]=new int [5];
 
-    System.out.println(a[8]);
+    System.out.println(a[8]); 
   }catch(Exception e){
     System.out.println("Hello guys");
   }
